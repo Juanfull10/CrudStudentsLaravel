35be052cee9ce1ae58b56a67b052be90a1e3f1c2
Juan Peraza <perazajuan770@gmail.com>
2025-11-07 13:11:08 -0500

    Primer commit del proyecto Laravel

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    
+    public function index()
+    {
+        $students=Student::all();
+        return view('students.index',compact('students'));
+       //return view('students.index')->with('students',$students);
+
+    }
+
+   
+    public function create()
+    {
+        return view('students.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name'=>'required|string|min:5|max:100',
+            'age'=>'required|string|min:1',
+        ]);
+
+        Student::create($request->all());
+
+        return redirect()->route('students.index'); 
+    }
+
+    public function show(string $id)
+    {
+        //
+    }
+
+    public function edit( string $id)
+    {
+        //
+       $student= Student::findOrFail($id);
+       
+       return view('students.edit',compact('student'));
+    }
+
+    public function update(Request $request, string $id)
+    {
+         $request->validate([
+            'name'=>'required|string|min:5|max:100',
+            'age'=>'required|string|min:1',
+        ]);
+        $student=Student::findOrFail($id);
+         $student->update($request->all());
+         return redirect()->route('students.index'); 
+ 
+    }
+
+    public function destroy(string $id)
+    {
+        //
+        $student=Student::findOrFail($id);
+        $student->delete();
+        return redirect()->route('students.index');
+    }
+}
